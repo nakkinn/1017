2747cdd2c49c798d34fce490371dcbee69b46679
nakkinn <81859514+nakkinn@users.noreply.github.com>
2024-10-18 00:41:42 +0900

    Add files via upload

--- script6.js
+++ script6.js
@@ -134,6 +134,7 @@ let utube_main_vts = tubeU_vtsC(func6, [y1], xrange6, detail2, tubethick*1.8, 6)
 let vtube_main_vts = tubeV_vtsC(func6, [x1], yrange6, detail2, tubethick*1.8, 6);
 
 
+
 //スライスのgc
 let slice1_vts, slice2_vts;
 slice1_vts = uslice_vtsC(func6, y1, xrange6, detail2, bottom_height);
@@ -153,6 +154,9 @@ let hashira2_vts = tube_vts1C(hashira2_core_vts, tubethick, 6);
 let hashira3_vts = tube_vts1C(hashira3_core_vts, tubethick, 6);
 let hashira4_vts = tube_vts1C(hashira4_core_vts, tubethick, 6);
 let hashira5_vts = tube_vts1C(hashira5_core_vts, tubethick*2, 6);
+
+alert(hashira1_core_vts);
+
 
 
 function scaleset(arg, sc){
@@ -203,13 +207,13 @@ addMeshC("setsuheimen_vts", setsuheimen_index, {color:0x00aa00, scale:scale1, op
 addMeshC(main_mesh_vts, main_mesh_mesh, {color:0xffff00, scale:scale1, opacity:"opacity6b", visible:"surface_visible"});    //曲面
 
 //四隅の柱
-addMeshC(hashira1_vts, uvtube_index0, {color:tubecolor2, scale:scale1});
-addMeshC(hashira2_vts, uvtube_index0, {color:tubecolor2, scale:scale1});
-addMeshC(hashira3_vts, uvtube_index0, {color:tubecolor2, scale:scale1});
-addMeshC(hashira4_vts, uvtube_index0, {color:tubecolor2, scale:scale1});
+addMeshC(hashira1_vts, uvtube_index0, {color:tubecolor2, scale:scale1, visible:"tube_visible"});
+addMeshC(hashira2_vts, uvtube_index0, {color:tubecolor2, scale:scale1, visible:"tube_visible"});
+addMeshC(hashira3_vts, uvtube_index0, {color:tubecolor2, scale:scale1, visible:"tube_visible"});
+addMeshC(hashira4_vts, uvtube_index0, {color:tubecolor2, scale:scale1, visible:"tube_visible"});
 
 //スライスが交わる箇所の柱
-addMeshC("hashira5_vts", uvtube_index0, {color:0xffffff, scale:scale1});
+addMeshC("hashira5_vts", uvtube_index0, {color:0xffffff, scale:scale1, visible:"slice_visible"});
 
 const check1 = document.getElementById("check1");
 const check2 = document.getElementById("check2");
