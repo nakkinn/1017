edf3def268a9059db7bf49f03869ecebc4f62047
nakkinn <81859514+nakkinn@users.noreply.github.com>
2024-11-07 10:26:40 +0900

    Add files via upload

--- common_cgweb2_b.js
+++ common_cgweb2_b.js
@@ -281,7 +281,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
         }
 
         camera.updateProjectionMatrix();
-        renderer.setSize(canvas.width, canvas.height);
+        renderer.setSize(canvas.style.width, canvas.style.height);
     }
 
     cgweb_setupC();
